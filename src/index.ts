5bb85c6e46c20b3fab1fffde9815855322baba64
vbonassies <valentin.bonassies@gmail.com>
2019-05-20 17:10:43 +0200

    add comment in index with instructions to run it

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {Pokemon} from "./Pokemon";
 import {allFight} from "./Fight";
 
+//to lanch, type tsc in the terminal and run the index.js file in the build folder
+
 let pokemonOne = new Pokemon("One", 10, 9, "fire", 30);
 let pokemonTwo = new Pokemon("Two", 5, 8, "water", 60);
 allFight(pokemonOne, pokemonTwo);
